3170593116acdbf195c5a034ea6380f75a702cec
Pragati121 <Pragati@infostride.com>
2022-11-07 20:46:11 +0530

    xmltostring

---
+++ src/test/java/PojoClass.java
@@ -0,0 +1,22 @@
+public class PojoClass  {
+    private String Firstname;
+    private String Lastname;
+
+    public String getFirstname() {
+        return Firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        Firstname = firstname;
+    }
+
+    public String getLastanme() {
+        return Lastname;
+    }
+
+    public void setLastanme(String lastname) {
+        Lastname = lastname;
+    }
+}
+
+
---
+++ src/test/java/TestClass.java
@@ -0,0 +1,14 @@
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+public class TestClass {
+        public static void main(String args[]) throws Exception {
+            XmlMapper xml = new XmlMapper();
+            String readContent = new String(Files.readAllBytes(Paths.get("C:\\Users\\PragatiSharma\\IdeaProjects\\ObjectOfObjectWithPojoClass\\Test.xml")));
+           PojoClass Test = xml.readValue(readContent, PojoClass.class);
+            String Name = Test.getFirstname();
+            String lName = Test.getLastanme();
+            System.out.println("Name = " + Name);
+            System.out.println("Lastname = "+lName);
+        }
+    }
